994c0430dddd3903f757c1b0a39a8743705bb6e1
jacklu-ncj <jack.lu@nocodejapan.org>
2023-02-18 14:03:27 -0800

    Add more examples to show how decorator works

---
+++ src/methodDecorators.ts
@@ -0,0 +1,60 @@
+interface Contact {
+  id: number;
+}
+
+const currentUser = {
+  id: 1234,
+  roles: ["ContactEditor"],
+  isAuthenticated(): boolean {
+    return true;
+  },
+  isInRole(role: string): boolean {
+    return this.roles.contains(role);
+  },
+};
+
+// Decorator factory
+function authorize(role: string) {
+  return function authorizeDecorator(
+    target: any,
+    property: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const wrapped = descriptor.value;
+    descriptor.value = function () {
+      if (!currentUser.isAuthenticated()) {
+        throw Error("User is not authenticated");
+      }
+      if (!currentUser.isInRole(role)) {
+        throw Error(`User not in role ${role}`);
+      }
+      try {
+        return wrapped(this, arguments);
+      } catch (ex) {
+        console.log(ex);
+        throw ex;
+      }
+    };
+  };
+}
+
+class ContactRepository {
+  private contacts: Contact[] = [];
+
+  @authorize("ContactViewer")
+  getContactById(id: number): Contact | null {
+    const contact = this.contacts.find((x) => x.id === id);
+    return contact;
+  }
+
+  @authorize("ContactEditor")
+  save(contact: Contact): void {
+    const existing = this.getContactById(contact.id);
+
+    if (existing) {
+      Object.assign(existing, contact);
+    } else {
+      this.contacts.push(contact);
+    }
+  }
+}
